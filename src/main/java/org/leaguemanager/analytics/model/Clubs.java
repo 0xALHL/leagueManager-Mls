c71e9c920bb666d1901ab11fb17440a5b0a432fa
0xALHL <amerehortonlangston@gmail.com>
2026-08-05 23:05:16 -0400

    Added Getters and Setters

--- src/main/java/org/leaguemanager/analytics/model/Clubs.java
+++ src/main/java/org/leaguemanager/analytics/model/Clubs.java
@@ -1,7 +1,7 @@
 package org.leaguemanager.analytics.model;
 
 public class Clubs {
-// need getters and Setters
+// Encapsulated instance variables
     private String clubName;
     private String location;
     private String stadium;
@@ -13,12 +13,45 @@ private enum Conference{
     WESTERN
 }
 
+    public void setconference(Conference conference) {
+        this.conference = conference;
+    }
+
+
 public Clubs(String clubName, String location, String stadium, Conference conference){
     this.clubName = clubName;
     this.location = location;
     this.stadium = stadium;
     this.conference = conference;
 }
+// Do not make getters private because it defeats the purpose
+public void setClubName(String clubName){
+    this.clubName = clubName;
+}
+
+public String getClubName(){
+    return this.clubName;
+}
+
+public void setlocation(String location){
+    this.location = location;
+}
+
+public String getlocation(){
+    return this.location;
+}
+
+public void setstadium(String stadium){
+    this.stadium = stadium;
+}
+
+public String getStadium(){
+    return this.stadium;
+}
+
+
+
+
 
 
 }
