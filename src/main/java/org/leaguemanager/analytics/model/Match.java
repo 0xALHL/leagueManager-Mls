b24851c3dbee8f48d3d11f37f06047803d7ec8d4
0xALHL <amerehortonlangston@gmail.com>
2026-08-12 12:32:18 -0400

    Began adding logic to the Match Class

--- src/main/java/org/leaguemanager/analytics/model/Match.java
+++ src/main/java/org/leaguemanager/analytics/model/Match.java
@@ -1,4 +1,23 @@
 package org.leaguemanager.analytics.model;
 
 public class Match {
+    private boolean won;
+    private boolean loss;
+    private boolean draw;
+
+    // My Team Options
+    private enum matchTeams{
+        homeTeam,
+        awayTeam
+    }
+
+    // My Reg Constructor
+    public Match(boolean won, boolean loss, boolean draw){
+        this.won = won;
+        this.loss = loss;
+        this.draw = draw;
+    }
+
+    // Wea going to need some basic logic for isWin, isLoss, isDraw and they will likely be boolean
+
 }
