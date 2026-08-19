5e9345462abfc7c509bf8463a9da50694d583a97
0xALHL <amerehortonlangston@gmail.com>
2026-08-19 13:27:06 -0400

    Added Starter Stat Variables

--- src/main/java/org/leaguemanager/analytics/model/TeamAnalytics.java
+++ src/main/java/org/leaguemanager/analytics/model/TeamAnalytics.java
@@ -1,4 +1,33 @@
 package org.leaguemanager.analytics.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class TeamAnalytics {
+
+    // Instance Variables for stats
+    //Offensive Variables
+    private int goals;
+    private int shotsTaken;
+    private int shotsOnTarget;
+    private int totalPassesAttempted;
+    private int totalPassesCompleted;
+    private int touches;
+    //Defensive Variables
+    private int tacklesWon;
+    private int interceptions;
+    private int foulsCommited;
+    private int yellowCards;
+    private int redCards;
+    private int saves;
+    //Team Goalie Variables
+    private int goalsAgainst;
+    private int cleanSheets;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
