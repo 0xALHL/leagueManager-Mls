5e1fdaf1cb40b8d0c67859df594ec2071cb036c3
0xALHL <amerehortonlangston@gmail.com>
2026-08-21 23:21:11 -0400

    Began the player analytics model need to also finish the Team Anaytics model

--- src/main/java/org/leaguemanager/analytics/model/PlayerAnalytics.java
+++ src/main/java/org/leaguemanager/analytics/model/PlayerAnalytics.java
@@ -1,4 +1,19 @@
 package org.leaguemanager.analytics.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+@Entity
 public class PlayerAnalytics {
+
+//Finish this up over the weekend now that all of the model entities have been touched
+    private double playerRating;
+    private int goals;
+
+
+
+
+
+    @Id
+    private Long id;
+
 }
